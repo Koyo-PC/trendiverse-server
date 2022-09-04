8007bcc1bf18133c0d7b31df9feaca3ddc94dc77
Blackjack8283 <d.ito0538@gmail.com>
2022-09-04 18:39:24 +0900

    track 0.3->0.5

--- node/twitter-api/api/track.js
+++ node/twitter-api/api/track.js
@@ -4,7 +4,7 @@ const getNameById = require('./getNameById.js');
 
 /**
  * track listに入っているもののうちtrend配列に含まれていないものを80trendsまで追加記録
- * 全区間での相対値が0.3未満になればtrackedに入る
+ * 全区間での相対値が0.5未満になればtrackedに入る
  * 
  * trend入ってるか確認->データ追加(->twitter_trackingからtwitter_trackedに移動)->twitter_trakingの空き確認->80まで新規追加
  * 
@@ -30,7 +30,7 @@ module.exports = async function track(trend){
             }
         }
 
-        //データ追加 & 0.3未満は削除して終了リストに追加
+        //データ追加 & 0.5未満は削除して終了リストに追加
         for(let i=0; i<track_list.length; i++){
             const num = await Twitter.count(track_list[i]["name"]);
             const max_obj = await DB.queryp(`select max(hotness) from twitter_trend${track_list[i]["id"]}`);
@@ -38,7 +38,7 @@ module.exports = async function track(trend){
 
             await DB.queryp(`insert into twitter_trend${track_list[i]["id"]} (hotness) values(${num})`);
 
-            if(num < 0.3 * max){
+            if(num < 0.5 * max){
                 await DB.queryp(`delete from twitter_tracking where id=${track_list[i]["id"]}`);
                 await DB.queryp(`insert into twitter_tracked (id) values(${track_list[i]["id"]})`);
             }
@@ -53,7 +53,11 @@ module.exports = async function track(trend){
         const tracking_num_obj = await DB.queryp(`select count(id) from twitter_tracking`);
         let tracking_num = tracking_num_obj["count(id)"];
 
-        for(let i=max_track_id+1; i<=max_trend_id; i++){
+        let start;
+        if(max_trend_id-10 <= max_track_id+1) start = max_track_id+1;
+        else start = max_trend_id-10;
+
+        for(let i=start; i<=max_trend_id; i++){
             if(tracking_num >= 80) break;
             tracking_num++;
             await DB.queryp(`insert into twitter_tracking (id) values(${i})`);
