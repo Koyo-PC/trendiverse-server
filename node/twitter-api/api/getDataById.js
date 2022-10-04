58531cd635caf628d1555c779958a6ed55aa6268
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-10-04 23:56:59 +0900

    change request lib

--- node/twitter-api/api/getDataById.js
+++ node/twitter-api/api/getDataById.js
@@ -1,5 +1,5 @@
 const DB = require("../../rest-api/db/TrendiverseDB.js");
-const request = require("request-promise");
+const request = require("request-promise-native");
 /**
  * table_id -> data
  * @param {int} id trend id
