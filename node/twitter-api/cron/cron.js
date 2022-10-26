8bfa250f1c28cffc027bae10fe7d6cc3be0c9a23
Blackjack8283 <d.ito0538@gmail.com>
2022-10-27 00:49:40 +0900

    token_type スコープミス

--- node/twitter-api/cron/cron.js
+++ node/twitter-api/cron/cron.js
@@ -13,18 +13,19 @@ function get_date_string(date=new Date()){
 }
 
 async function main(){
+    const min = new Date().getMinutes();
+    let token_type;
+    if((0 <= min && min < 5) || (20 <= min && min < 25) || (40 <= min && min < 45)){
+        token_type = 1;
+    } else if((5 <= min && min < 10) || (25 <= min && min < 30) || (45 <= min && min < 50)){
+        token_type = 2;
+    } else if((10 <= min && min < 15) || (30 <= min && min < 35) || (50 <= min && min < 55)){
+        token_type = 3;
+    } else {
+        token_type = 4;
+    }
+    
     try {
-        const min = new Date().getMinutes();
-        let token_type;
-        if((0 <= min && min < 5) || (20 <= min && min < 25) || (40 <= min && min < 45)){
-            token_type = 1;
-        } else if((5 <= min && min < 10) || (25 <= min && min < 30) || (45 <= min && min < 50)){
-            token_type = 2;
-        } else if((10 <= min && min < 15) || (30 <= min && min < 35) || (50 <= min && min < 55)){
-            token_type = 3;
-        } else {
-            token_type = 4;
-        }
         await TwitterAPI.getTrend(token_type);
         console.log(get_date_string()+" successfully added all trend data with token" + token_type);
         fs.appendFileSync( "./cron.log" , get_date_string()+": successfully added all trend data with token" + token_type + "\n" );
@@ -34,7 +35,7 @@ async function main(){
     }
 }
 
-// 毎時0,20,40, 10,30,50 分
+// 毎時0,20,40,5,25,45,10,30,50,15,35,55 分
 cron.schedule('0,20,40,5,25,45,10,30,50,15,35,55 * * * *', () => {
     main();    
 }, {
