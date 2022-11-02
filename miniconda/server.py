0b773c67e78efb1bbef66918ec9d4ddecb0e686c
zeel0 <zen050906@gmail.com>
2022-11-02 16:40:43 +0900

    getDataで一重になるやつを解消

--- miniconda/server.py
+++ miniconda/server.py
@@ -46,7 +46,7 @@ class StubHttpRequestHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(encoded)
                 return
-            str_result ='{"id": ' + str(result[0]) + ', "data": ' + str(pd.DataFrame({"date": result[1].keys(), "hotness": result[1].values()}).to_json(orient="records")) + '}'
+            str_result ='{"id": ' + str(result[0]) + ', "data": ' + str(result[1]) + '}'
         encoded = str_result.encode(enc, 'suurogateescape')
 
         self.send_response(HTTPStatus.OK)
@@ -60,17 +60,23 @@ class StubHttpRequestHandler(BaseHTTPRequestHandler):
 # ↓↓↓ この関数変えてほしい〜〜〜〜〜
 
 def getData(trend_id: int) -> Tuple[int, Dict[str, int]] | None:
-    r = requests.get(f"http://172.30.0.10:8081/getDataById?id={trend_id}")
-    json_r = json.loads(r.text)
-    df = pd.DataFrame(json_r["list"])
-    if df.empty: return None
-    df["date"] = df["date"].map(convert_datetime)
-    date, hotness = make_diff_five(df["date"].tolist(), np.array(df["hotness"]).astype(np.float64))
-    date = pd.Series(date).map(str).to_list()
-    hotness = hotness.astype(int).tolist()
-    if len(date) == 1:
+    r = requests.get(f"http://172.30.0.10:8081/getDividedDataById?id={trend_id}")
+    part_list = json.loads(r.text)["list"]
+    parts = len(part_list)
+    if len(parts) == 0:
         return None
-    return trend_id, dict(zip(date, hotness))
+    ret: List[List[Dict[str, int]]] = list(range(parts))
+    for i in range(parts):
+        df = pd.DataFrame(part_list[i])
+        date = df["date"].map(convert_datetime).tolist()
+        hotness = np.array(df["hotness"]).astype("float")
+        if len(date) == 0:
+            return None
+        date, hotness = make_diff_five(date, hotness)
+        ret[i] = pd.DataFrame(np.array([pd.Series(date).map(str).to_list(), hotness.tolist()]).T, columns=["date", "hotness"]).to_dict("records")
+    if len(ret) == 0:
+        return None
+    return trend_id, ret
 
 if __name__ == '__main__':
     handler = StubHttpRequestHandler
